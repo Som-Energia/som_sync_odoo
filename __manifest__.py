3b9099de05533eaddc65174ebbdace57a9a18592
Som Energia - IT Crowd <itcrowd@somenergia.coop>
2021-02-12 16:54:50 +0000

    WIP: added openerp_id field in modules

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,9 @@
 {
-    'name': 'som_sync_openerp',
+    'name': 'Som Sync Odoo OpenERP',
     'version': '0.1',
     'category': 'Tools',
     'summary': 'Sync OpenERP  Odoo',
-    'depends': ['base'],
+    'depends': ['base', 'account'],
     'data': [],
     'installable': True,
     'auto_install': True,
