feee25fd3ae7c1fb0b336b8ae4bc0519fe68a0c9
Som Energia - IT Crowd <itcrowd@somenergia.coop>
2021-02-12 12:51:50 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'som_sync_openerp',
+    'version': '0.1',
+    'category': 'Tools',
+    'summary': 'Sync OpenERP  Odoo',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+}
