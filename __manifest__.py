82014555f4e3bcf4f8f128ce543bb99e08f390f4
susu105 <davidsuarez92@gmail.com>
2021-05-13 16:06:48 +0200

    upg version to v12.0.0.0.1 and CHANGELOG added

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,21 @@
 {
     'name': 'Som Sync Odoo OpenERP',
-    'version': '0.1',
+    'version': '12.0.0.0.1',
     'category': 'Tools',
     'summary': 'Sync OpenERP  Odoo',
     'depends': ['base', 'account'],
-    'data': [],
+    'author': "SomEnergia",
+    'website': 'https://www.somenergia.coop',
+    'category': "Cooperative management",
+    'description': """ 
+    Odoo - OpenERP Som Energia Sync module.
+    """,
+    "license": "AGPL-3",
+    'demo': [
+    ],
+    'data': [
+    ],
     'installable': True,
-    'auto_install': True,
+    'application': True,
+    'auto_install': False
 }
